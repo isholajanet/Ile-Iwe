d06d24f7184bd38a5457be9809df9275e38fc387
Janet Ishola <janetishola2010@gmail.com>
2021-10-28 01:27:16 +0100

    db connection

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,6 +1,19 @@
 create table student(id INT primary key, firstname VARCHAR(45), lastname VARCHAR(45),
                         dob DATE, gender VARCHAR(45))
 
-insert into student(`id`, `firstname`, `lastname`, `dob`, `gender`)
-values(110, 'janet', 'ishola', '28/10/2021', 'Female'),
-      (111, 'dunni', 'olayemi', '30/01/2020', 'Female');
+insert into student(`id`, `firstname`, `lastname`, `gender`)
+values(110, 'janet', 'ishola', 'Female'),
+      (111, 'dunni', 'olayemi', 'Female');
+
+insert into instructor(`id`, `firstname`, `lastname`, `gender`, `specialization`, `bio`)
+values(110, 'janet', 'ishola', 'Female', 'Tech', 'A tech guru'),
+      (111, 'dunni', 'olayemi', 'Female', 'BioChem', 'Micro organisms');
+
+insert into learning_party(`id`, `email`, `password`, `enabled`)
+values(110, 'janet@gmail.com', 'janet123', true),
+      (111, 'dunni@gmail.com', 'olayemi12', false);
+
+insert into course(`id`, `title`, `description`, `duration`, `language`, `bio`)
+values(110, 'Python', 'A programming language', '3 months', 'French'),
+      (111, 'Java', 'A programming language', '1 year', 'English');
+
